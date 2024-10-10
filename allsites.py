bb2ba67c2d9c1f36bec5272bf664b278b39f87af
rakaarfi <rakaarfi@gmail.com>
2024-10-10 13:34:47 +0700

    adding all site from kompas and detik

--- allsites.py
+++ allsites.py
@@ -1,13 +1,89 @@
-#news
-DETIK_NEWS = "https://news.detik.com/indeks/"
-KOMPAS_NEWS = "https://indeks.kompas.com/?site=news"
-
-#finance
-DETIK_BUSINESS = "https://finance.detik.com/indeks/"
-KOMPAS_MONEY = "https://indeks.kompas.com/?site=money"
-KOMPAS_UMKM = "https://indeks.kompas.com/?site=umkm"
-
 #top news
 DETIK_TOPNEWS = "https://hot.detik.com/indeks/"
 KOMPAS_TREN = "https://indeks.kompas.com/?site=tren"
 KOMPAS_HYPE = "https://indeks.kompas.com/?site=hype"
+
+#politics
+
+
+#business
+DETIK_BUSINESS = "https://finance.detik.com/indeks/"
+KOMPAS_MONEY = "https://indeks.kompas.com/?site=money"
+KOMPAS_UMKM = "https://indeks.kompas.com/?site=umkm"
+
+#sports
+DETIK_SPORT = "https://sport.detik.com/indeks/"
+DETIK_BOLA = "https://sport.detik.com/sepakbola/indeks/"
+DETIK_ALL_SPORT = [DETIK_SPORT, DETIK_BOLA]
+
+KOMPAS_BOLA = "https://indeks.kompas.com/?site=bola"
+
+#entertainment
+DETIK_POP = "https://www.detik.com/pop/indeks/"
+
+
+#health
+DETIK_HEALTH = "https://health.detik.com/indeks/"
+KOMPAS_HEALTH = "https://indeks.kompas.com/?site=health"
+
+#science and tech
+DETIK_INET = "https://inet.detik.com/indeks/"
+KOMPAS_TEKNO = "https://indeks.kompas.com/?site=tekno"
+KOMPAS_SAINS = "https://indeks.kompas.com/?site=sains"
+
+#news
+DETIK_NEWS = "https://news.detik.com/indeks/"
+KOMPAS_NEWS = "https://indeks.kompas.com/?site=news"
+
+
+##worldnews
+KOMPAS_GLOBAL = "https://indeks.kompas.com/?site=global"
+
+
+##localnews
+DETIK_JATIM = "https://www.detik.com/jatim/indeks/"
+DETIK_JATENG = "https://www.detik.com/jateng/indeks/"
+DETIK_JABAR = "https://www.detik.com/jabar/indeks/"
+DETIK_SULSEL = "https://www.detik.com/sulsel/indeks/"
+DETIK_SUMUT = "https://www.detik.com/sumut/indeks/"
+DETIK_BALI = "https://www.detik.com/bali/indeks/"
+DETIK_SUMBAGSEL = "https://www.detik.com/sumbagsel/indeks/"
+DETIK_JOGJA = "https://www.detik.com/jogja/indeks/"
+
+DETIK_LOCALNEWS = [DETIK_JATIM, DETIK_JATENG, DETIK_JABAR, DETIK_SULSEL, DETIK_SUMUT, DETIK_BALI, DETIK_SUMBAGSEL, DETIK_JOGJA]
+
+KOMPAS_NASIONAL = "https://indeks.kompas.com/?site=nasional"
+KOMPAS_REGIONAL = "https://indeks.kompas.com/?site=regional"
+KOMPAS_MEGAPOLITAN = "https://indeks.kompas.com/?site=megapolitan"
+
+
+
+#travel
+DETIK_TRAVEL = "https://travel.detik.com/indeks/"
+DETIK_FOOD = "https://food.detik.com/indeks/"
+DETIK_ALL_TRAVEL = [DETIK_TRAVEL, DETIK_FOOD]
+
+KOMPAS_FOOD = "https://indeks.kompas.com/?site=food"
+KOMPAS_TRAVEL = "https://indeks.kompas.com/?site=travel"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
