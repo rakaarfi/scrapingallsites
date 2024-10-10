1ee99b3af88a85806d0e378768fee91e139553f4
rakaarfi <rakaarfi@gmail.com>
2024-10-10 14:12:26 +0700

    modifying code

--- verceltest.py
+++ verceltest.py
@@ -1,42 +1,29 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from io import StringIO
 import pandas as pd
 
 # Dummy dictionary
-dummy_data = {
+dummy_data = [{
     "name": "John Doe",
     "age": 29,
     "occupation": "Software Developer",
     "email": "johndoe@example.com",
-    "skills": ["Python", "JavaScript", "React", "Django"],
-    "experience": {
-        "company_1": {
-            "name": "Tech Corp",
-            "role": "Frontend Developer",
-            "years": 2
-        },
-        "company_2": {
-            "name": "DevSolutions",
-            "role": "Full Stack Developer",
-            "years": 1
-        }
-    },
-    "hobbies": ["Reading", "Traveling", "Photography"]
-}
+}]
 
 app = FastAPI()
 
 @app.get("/")
 def testing():
   dummy_df = pd.DataFrame(dummy_data)
+  
   csv_buffer = StringIO()
-  dummy_df.to_csv(csv_buffer, index=False)
+  dummy_df.to_csv(csv_buffer)
   csv_buffer.seek(0)
 
   csv_data = "dummy.csv"
   
-  return  FileResponse(
+  return StreamingResponse(
         csv_buffer,
         media_type="text/csv",
         headers={"Content-Disposition": f"attachment; filename={csv_data}"}
