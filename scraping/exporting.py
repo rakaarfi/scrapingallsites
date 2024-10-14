afa31bdb1cf91f1402d41f56e1ab440904246a29
rakaarfi <rakaarfi@gmail.com>
2024-10-14 15:28:07 +0700

    add function to generate category and its link

--- scraping/exporting.py
+++ scraping/exporting.py
@@ -13,6 +13,20 @@ def create_dict(resource, count, data):
         "Count": count,
         "Generated Time": generated_time,
         "Data": data
-    }
+    }                                                                                      
+    return result
 
+# Dictionary to show categories and link from each source
+def generate_result(links_data, categories):
+    result = {}
+    # Looping each source(detik, kompas and tribun)
+    # Data is categories and its link
+    for source, data in links_data.items():
+        source_categories = []
+        # Categories is list of categories
+        for category in categories:
+            # If there is a category, get the link
+            # If not, None
+            source_categories.append({category: data.get(category, None)})
+        result[source] = {'categories': source_categories}
     return result
