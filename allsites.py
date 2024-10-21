cde34f22968b7791df80d9ca3144e2c5f65f46b4
rakaarfi <rakaarfi@gmail.com>
2024-10-21 10:02:07 +0700

    change data type from str to list or array

--- allsites.py
+++ allsites.py
@@ -1,5 +1,5 @@
 #top news
-DETIK_TOPNEWS = "https://hot.detik.com/indeks/"
+DETIK_TOPNEWS = ["https://hot.detik.com/indeks/"]
 KOMPAS_ALL_TOPNEWS = ["https://indeks.kompas.com/?site=tren", "https://indeks.kompas.com/?site=hype"]
 
 #politics
@@ -11,31 +11,31 @@ TRIBUN_ALL_BUSINESS = ['https://www.tribunnews.com/bisnis', 'https://www.tribunn
 
 #sports
 DETIK_ALL_SPORT = ["https://sport.detik.com/indeks/", "https://sport.detik.com/sepakbola/indeks/"]
-KOMPAS_BOLA = "https://indeks.kompas.com/?site=bola"
+KOMPAS_BOLA = ["https://indeks.kompas.com/?site=bola"]
 TRIBUN_ALL_SPORT = ['https://www.tribunnews.com/superskor', 'https://www.tribunnews.com/sport']
 
 #entertainment
-DETIK_POP = "https://www.detik.com/pop/indeks/"
+DETIK_POP = ["https://www.detik.com/pop/indeks/"]
 TRIBUN_ALL_ENTERTAINMENT = ['https://www.tribunnews.com/seleb', 'https://www.tribunnews.com/musik']
 
 #health
-DETIK_HEALTH = "https://health.detik.com/indeks/"
-KOMPAS_HEALTH = "https://indeks.kompas.com/?site=health"
-TRIBUN_HEALTH = 'https://www.tribunnews.com/kesehatan'
+DETIK_HEALTH = ["https://health.detik.com/indeks/"]
+KOMPAS_HEALTH = ["https://indeks.kompas.com/?site=health"]
+TRIBUN_HEALTH = ['https://www.tribunnews.com/kesehatan']
 
 #science and tech
-DETIK_INET = "https://inet.detik.com/indeks/"
+DETIK_INET = ["https://inet.detik.com/indeks/"]
 KOMPAS_ALL_SCIENCETECH = ["https://indeks.kompas.com/?site=sains", "https://indeks.kompas.com/?site=tekno"]
-TRIBUN_TECH = 'https://www.tribunnews.com/techno'
+TRIBUN_TECH = ['https://www.tribunnews.com/techno']
 
 #news
-DETIK_NEWS = "https://news.detik.com/indeks/"
-KOMPAS_NEWS = "https://indeks.kompas.com/?site=news"
-TRIBUN_NEWS = 'https://www.tribunnews.com/news'
+DETIK_NEWS = ["https://news.detik.com/indeks/"]
+KOMPAS_NEWS = ["https://indeks.kompas.com/?site=news"]
+TRIBUN_NEWS = ['https://www.tribunnews.com/news']
 
 ##worldnews
-KOMPAS_GLOBAL = "https://indeks.kompas.com/?site=global"
-TRIBUN_INTERNASIONAL = 'https://www.tribunnews.com/internasional'
+KOMPAS_GLOBAL = ["https://indeks.kompas.com/?site=global"]
+TRIBUN_INTERNASIONAL = ['https://www.tribunnews.com/internasional']
 
 ##localnews
 DETIK_ALL_LOCALNEWS = ["https://www.detik.com/jatim/indeks/", "https://www.detik.com/jateng/indeks/", "https://www.detik.com/jabar/indeks/", "https://www.detik.com/sulsel/indeks/", "https://www.detik.com/sumut/indeks/", "https://www.detik.com/bali/indeks/", "https://www.detik.com/sumbagsel/indeks/", "https://www.detik.com/jogja/indeks/"]
@@ -45,4 +45,4 @@ TRIBUN_ALL_LOCALNEWS = ['https://www.tribunnews.com/nasional', 'https://www.trib
 #travel
 DETIK_ALL_TRAVEL = ["https://travel.detik.com/indeks/", "https://food.detik.com/indeks/"]
 KOMPAS_ALL_TRAVEL = ["https://indeks.kompas.com/?site=food", "https://indeks.kompas.com/?site=travel"]
-TRIBUN_TRAVEL = 'https://www.tribunnews.com/travel'
+TRIBUN_TRAVEL = ['https://www.tribunnews.com/travel']
